5cb1b393e2fdaf6ad5e3bae97417295f2a905c50
TEC- Julio Amaro Alves Junior <junior_julio@unimedlondrina.com.br>
2023-10-06 18:23:15 -0300

    continuacao da aula

--- exames/views.py
+++ exames/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import *
+from datetime import datetime
 
 # Create your views here.
 
@@ -25,7 +26,26 @@ def Solicitar_exames(request):
                                                        'solicitacao_exames':solicitacao_exames,
                                                        'preco_total': preco_total })
     
-
+@login_required
 def Fechar_pedido(request):
-    print(request.POST)
+    exames_id = request.POST.getlist('exames')
+    solicitacao_exames = TipoExames.objects.filter(id__in =exames_id)
+
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now()
+    )
+    pedido_exame.save()
+
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp = SolicitacaoExame(
+            usuario = request.user,
+            exame = exame,
+            status = "E"
+        )
+        solicitacao_exames_temp.save()
+        pedido_exame.exames.add(solicitacao_exames_temp)
+
+    pedido_exame.save()
+
     return HttpResponse('teste')
